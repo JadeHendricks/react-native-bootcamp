3192165be23b427f08660b3fd77dd8dfd4db090b
Jade <hendricksjade@gmail.com>
2024-05-10 15:21:16 +0200

    add(bottom-tabs): adding in the bottom tabs, added in styling for the tab bar and focus/active states.

--- app/index.jsx
+++ app/index.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-black">Aora!</Text>
-      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
+      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
       <StatusBar style="auto" />
     </View>
   );
