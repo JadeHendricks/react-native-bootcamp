d39bfc8ca870eed26fda6395503eeb045174cd71
Jade <hendricksjade@gmail.com>
2024-05-12 20:40:08 +0200

    add(appwrite): implementing app write to create user sessions and video/image uploading. Added in .env to hide information.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { useEffect } from 'react';
-import { SplashScreen, Stack } from 'expo-router';
-import { useFonts } from 'expo-font';
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
 
-//this will prevent the SplahScreen from auto hiding before asset loading is complete
+import GlobalProvider from "../context/GlobalProvider";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-  //a special hook, takes in fontsLoaded and error if there is an error loading the fonts
   const [fontsLoaded, error] = useFonts({
     "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
     "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
@@ -19,28 +20,33 @@ const RootLayout = () => {
     "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
     "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
   });
-  
+
   useEffect(() => {
     if (error) throw error;
-  
+
     if (fontsLoaded) {
-      //hides the native splace screen immediately
       SplashScreen.hideAsync();
     }
   }, [fontsLoaded, error]);
-  
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   if (!fontsLoaded && !error) {
     return null;
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}}/>
-      <Stack.Screen name="(auth)" options={{headerShown: false}}/>
-      <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-      {/* <Stack.Screen name="/search/[query]" options={{headerShown: false}}/> */}
-    </Stack>
-  )
-}
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
+      </Stack>
+    </GlobalProvider>
+  );
+};
 
 export default RootLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -1,50 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { ScrollView, Text, View, Image } from 'react-native';
-import { Redirect, router } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { images } from '../constants';
-import CustomButton from '../components/CustomButton';
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function App() {
+import { images } from "../constants";
+import { CustomButton, Loader } from "../components";
+import { useGlobalContext } from "../context/GlobalProvider";
+
+const Welcome = () => {
+  const { loading, isLogged } = useGlobalContext();
+
+  if (!loading && isLogged) return <Redirect href="/home" />;
+
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{height: "100%"}}>
-        <View className="w-full justify-center items-center min-h-[85vh] px-4">
-          <Image 
+      <Loader isLoading={loading} />
+
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
             resizeMode="contain"
           />
-          <Image 
+
+          <Image
             source={images.cards}
-            className="max-w-[380px] w-full h-[300px]"
+            className="max-w-[380px] w-full h-[298px]"
             resizeMode="contain"
           />
+
           <View className="relative mt-5">
             <Text className="text-3xl text-white font-bold text-center">
-              Discover Endless Posibilities with <Text className="text-secondary-200">Aora</Text>
+              Discover Endless{"\n"}
+              Possibilities with{" "}
+              <Text className="text-secondary-200">Aora</Text>
             </Text>
-            <Image 
+
+            <Image
               source={images.path}
               className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
               resizeMode="contain"
             />
           </View>
-          <Text className="text-sm text-gray-100 font-pregular mt-7 text-center">
-             Where creativity meets innovation: Embark on a journey of limitless exploration with Aora
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where Creativity Meets Innovation: Embark on a Journey of Limitless
+            Exploration with Aora
           </Text>
-          <CustomButton 
-            title="Continue with email"
-            handlePress={() => {router.push('/sign-in')}}
+
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
       </ScrollView>
-      {/* this will darken your feed on your phone so it's less noticable: battery percentage etc */}
-      <StatusBar 
-        backgroundColor="#161622"
-        style="light"
-      />
+
+      <StatusBar backgroundColor="#161622" style="light" />
     </SafeAreaView>
   );
-}
+};
+
+export default Welcome;
